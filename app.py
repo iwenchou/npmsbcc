bf363154076c879d1d694ac527a1c410a3fb9fdb
iwenchou <142210075+iwenchou@users.noreply.github.com>
2023-08-14 21:26:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,25 @@ openai.api_key = "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"
 def index():
     return render_template("index.html")
 
+@app.route("/generate_response", methods=["POST"])
+def generate_response():
+    category = request.json["category"]
+    feedback = request.json["feedback"]
+    investigation = request.json["investigation"]
+
+    assistant_response = generate_openai_response(category, feedback, investigation)
+
+    return jsonify({"response": assistant_response})
+    
+     # 將助理回應加入聊天歷史中
+    chat_history = [{"role": "system", "content": system_message}]
+    user_message = f"觀眾意見類別：{category}\n觀眾意見內容：{feedback}\n業管單位調查：{investigation}\n"
+    chat_history.append({"role": "user", "content": user_message})
+    chat_history.append({"role": "assistant", "content": assistant_response})
+
+    # 將整個聊天歷史轉換成 JSON 格式回傳給前端
+    return jsonify({"chat_history": chat_history})
+
 def generate_openai_response(category, feedback, investigation):
     system_message = "現在開始你將擔任國立故宮博物院南部院區（故宮南院）的資深博物館館員，除了要熟捻中國歷史與亞洲藝術史，文化人類學等相關研究領域，還有博物館學典藏、文物維護、展示、教育等相關專業知識，並了解故宮南院官方的回覆綱領，以溫柔堅定並且專業的口吻，表達1. 誠懇的致歉 2.說明理由或解決方案 3. 感謝觀眾諒解並期待觀眾再度來訪光臨。"
     user_message = f"觀眾意見類別：{category}\n觀眾意見內容：{feedback}\n業管單位調查：{investigation}\n"
@@ -25,8 +44,6 @@ def generate_openai_response(category, feedback, investigation):
         max_tokens=500  # Adjust this value to control the response length
     )
 
-    assistant_response = response.choices[0].message["content"]
-    return assistant_response
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
