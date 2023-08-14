ce7ff41914d9a0cf3c6d33d7a50d9de520ddb81d
iwenchou <142210075+iwenchou@users.noreply.github.com>
2023-08-14 21:20:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import openai
 import os
+import threading
 
 app = Flask(__name__)
 
@@ -10,12 +11,7 @@ openai.api_key = "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"
 def index():
     return render_template("index.html")
 
-@app.route("/generate_response", methods=["POST"])
-def generate_response():
-    category = request.form.get("category")
-    feedback = request.form.get("feedback")
-    investigation = request.form.get("investigation")
-
+def generate_openai_response(category, feedback, investigation):
     system_message = "現在開始你將擔任國立故宮博物院南部院區（故宮南院）的資深博物館館員，除了要熟捻中國歷史與亞洲藝術史，文化人類學等相關研究領域，還有博物館學典藏、文物維護、展示、教育等相關專業知識，並了解故宮南院官方的回覆綱領，以溫柔堅定並且專業的口吻，表達1. 誠懇的致歉 2.說明理由或解決方案 3. 感謝觀眾諒解並期待觀眾再度來訪光臨。"
     user_message = f"觀眾意見類別：{category}\n觀眾意見內容：{feedback}\n業管單位調查：{investigation}\n"
 
@@ -26,11 +22,11 @@ def generate_response():
         model="gpt-3.5-turbo",
         messages=chat_history,
         temperature=0.38,
-        max_tokens=400  # Adjust this value to control the response length
+        max_tokens=500  # Adjust this value to control the response length
     )
 
     assistant_response = response.choices[0].message["content"]
-    return render_template("index.html", response=assistant_response)
+    return assistant_response
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
