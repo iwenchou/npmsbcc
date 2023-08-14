83c22a24365f9c539e6b31f8d5b0934fb7dfb8e6
iwenchou <142210075+iwenchou@users.noreply.github.com>
2023-08-14 19:59:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import openai
+import os
 
 app = Flask(__name__)
 
@@ -33,5 +34,6 @@ def generate_response():
     return render_template("index.html", response=official_response)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
