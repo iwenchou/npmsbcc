241dc6473c2f2982138108598b4a8de8c41db506
iwenchou <142210075+iwenchou@users.noreply.github.com>
2023-08-14 18:29:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,27 +14,38 @@ handler = WebhookHandler('e34a22c11a857de4dec2c631b43e59d2')
 # 設定您的 ChatGPT API 存取權杖
 openai.api_key = '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'
 
+def generate_official_response(feedback, initial_response):
+    prompt = f"觀眾反應：{feedback}\n業管單位的初步回應：{initial_response}\n正式回覆："
+    
+    response = openai.Completion.create(
+        model="text-davinci-003",  # 或其他您喜歡的模型
+        prompt=prompt,
+        max_tokens=150-300  # 您可以根據需要調整生成回覆的長度
+    )
+    
+    official_response = response.choices[0].text.strip()
+    return official_response
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 從使用者取得訊息內容
     user_message = event.message.text
-
-    # 使用 ChatGPT 生成回覆
-    chatgpt_response = generate_chatgpt_response(user_message)
-
-    # 回覆 ChatGPT 生成的訊息
+    feedback = "一個國家級博物館，北邊等車排隊的地方用小石子鋪路，請問是沒有經費了嗎?!"
+    initial_response = "(1)現為碎石子鋪面之排隊動線處(3m*12m)，將更換為透水磚地坪。"
+    chatgpt_response = generate_chatgpt_response(feedback, initial_response)
     reply_message = TextSendMessage(text=chatgpt_response)
     line_bot_api.reply_message(event.reply_token, reply_message)
 
-def generate_chatgpt_response(input_text):
-    # 調用 ChatGPT API 生成回覆
+def generate_chatgpt_response(feedback, initial_response):
+    prompt = f"觀眾反應：{feedback}\n業管單位的初步回應：{initial_response}\n正式回覆："
+    
     response = openai.Completion.create(
-        engine="davinci",  # 選擇適合的引擎
-        prompt=input_text,
-        max_tokens=50  # 控制回覆長度
+        engine="text-davinci-003",  # 或其他您喜歡的模型
+        prompt=prompt,
+        max_tokens=150  # 您可以根據需要調整生成回覆的長度
     )
-    return response.choices[0].text.strip()
-
+    
+    official_response = response.choices[0].text.strip()
+    return official_response
 @app.route('/callback', methods=['POST'])
 def callback():
     # 獲取 X-Line-Signature 進行驗證
